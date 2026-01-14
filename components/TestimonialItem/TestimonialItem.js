4666282714d820b1a7b44d6ae0f292e9d0864b37
Brian Chenoweth <brian.michael.chenoweth@gmail.com>
2026-01-13 18:29:01 -0800

    Remove author

--- components/TestimonialItem/TestimonialItem.js
+++ components/TestimonialItem/TestimonialItem.js
@@ -7,16 +7,13 @@ const cx = className.bind(styles);
  * Render the testimonial item component
  *
  * @param {Props} props The props object.
- * @param {string} props.author The author of the testimonial.
  * @param {React.ReactElement} props.children The content of the testimonial.
  * @returns {React.ReactElement} The testimonial item component.
  */
-export default function TestimonialItem({ author, children }) {
+export default function TestimonialItem({ children }) {
   return (
     <div className={cx('container')}>
       <div className={cx('content')}>{children}</div>
-
-      {/* {author && <p className={cx('author')}>{author}</p>} */}
     </div>
   );
 }
