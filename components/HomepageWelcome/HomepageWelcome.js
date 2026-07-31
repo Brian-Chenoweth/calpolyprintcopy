0c6fe21defdcc29eb7dca41ea79365c0a3fe5caf
Brian Chenoweth <brian.michael.chenoweth@gmail.com>
2026-07-30 21:28:08 -0700

    Edit Homepage Welcome text

--- components/HomepageWelcome/HomepageWelcome.js
+++ components/HomepageWelcome/HomepageWelcome.js
@@ -7,14 +7,11 @@ export default function HomepageWelcome() {
         <div className={styles.panel}>
           <div className={styles.content}>
             <h2 className={styles.heading}>
-              <span>Bring Your Ideas&nbsp;</span>
-              <span className={styles.highlight}>To Life</span>
+              <span>We Make Learn By Doing</span>
+              <span className={styles.highlight}>Visible</span>
             </h2>
             <p className={styles.lead}>
-              Need a poster for your research project? Flyers for a club event? A banner for game day? We've got you covered.
-            </p>
-            <p className={styles.body}>
-              Alumni and students are joining Cal Poly Career Connections to make online networking easier! Now is the time to make a connection. Mentor a student or make new connections with fellow alumni, all in one free and easy-to-use platform.
+              Cal Poly Print & Copy helps turn your ideas into polished, professional materials that make an impact. Whether you need a poster for your research project, flyers for a club event, a banner for game day, presentation boards, or promotional materials, we're your on-campus print partner. With high-quality printing, quick turnaround, and friendly support, we make it easy to showcase your work and bring your Learn by Doing experience to life.
             </p>
           </div>
         </div>
